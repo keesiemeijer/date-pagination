6a688cfca79c9a2349d82f4ee4ddf638d14145b3
keesiemeijer <keesie.meijer@gmail.com>
2017-01-27 16:21:39 +0100

    Update for travis-ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,9 @@
  * @package Date_Pagination_Unit_Tests
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+
+$_tests_dir = '/tmp/wordpress-tests-lib';
+
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
